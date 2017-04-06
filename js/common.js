70099b6d7ef87f0b2472b2ddd269618d89242258
kutepovvl <kutepovvl56@gmail.com>
2017-04-06 10:29:11 +0500

    commit\

--- js/common.js
+++ js/common.js
@@ -1,20 +1,45 @@
 $(function() {
-
-	$('.main-head, .testimonials').stellar();
-
     
-    $(".toggle-menu").click(function() {
-        $(this).toggleClass("on");
-        $(".navbar-menu").slideToggle();
+    $("body").niceScroll({
+        horizrailenabled: false
     });
     
-    $('.testimonials-carousel').owlCarousel({
-        loop: true,
-        items: 1,
-        itemClass: "test",
-		navText: "",
-		autoplay: true,
-        autoplayTimeout: 3000
+    $(".btn-bottom").click(function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор блока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+    
+    $(".services-item").equalHeights();
+    
+    $(".works").mixItUp({
+        selectors: {
+            target:'.mix',
+            filter:'.tab'
+        }, 
+        animation: {
+            enable: true,
+            effects: 'scale fade',
+            duration: 700
+        }
     });
+    
+    $(".portfolio-item").magnificPopup({
+        type : 'image',
+        removalDelay: 300,
+        mainClass: 'mfp-fade'
+    });
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
 });
+
+
